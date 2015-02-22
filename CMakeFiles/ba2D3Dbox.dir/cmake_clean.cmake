ecdfeafba24f140dfec89d5a2a30311fdf9d1057
prtksnghl <prtk.snghl@gmail.com>
2015-02-22 14:50:30 -0500

    adding the changed box_synthetic

---
+++ CMakeFiles/ba2D3Dbox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ba2D3Dbox.dir/ba2D3Dbox.cc.o"
+  "ba2D3Dbox.pdb"
+  "ba2D3Dbox"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ba2D3Dbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- box_synthetic.m
+++ box_synthetic.m
@@ -16,126 +16,126 @@ if nargin < 4
 	save_filename	=	'recon_error.log';
 end
 
-noise_std	=	[1 2 3 4 5];																% Gaussian noise std in points.
-dim_num		=	3;																			% Dimension
-num_pts		=	1000;																		% number of points
-seed = 123456789;
-pts_scale	=	2.5;																		% Scale points with this factor after generation.
-pts_on_cube	=	pts_scale * uniform_on_cube( dim_num, num_pts, seed );						% Generated random points on a cube.
-sigma		=	noise_std( noise_num );
-K			=	[718.856 0 607.1928; 0 718.856 185.2157; 0 0 1];							% Camera intrinsic parameters.
-w3D 		=	10;																			% Some threshold passed to ceres.
-
-debug_flag	=	0;
-debug_impts	=	0;
-num_views	=	15;																			% Number of camera views.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% IGNORE THIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     if matlabpool('size') ==0
-%         try
-%             matlabpool open 2;
-%         catch
-%         end
-%     end
-% random points generator on a cube
-% system('rm myFile.txt');
-% load('cube.mat');
-% for num=1:size(t,2)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% IGNORE THIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Random intilization of the trajectories  without rotations 
-projection_matrices(:,:,1)	=	[1 0 0 0.5;0 1 0 0.2; 0 0 1 20  ;0 0 0 1];					% z=0
-projection_matrices(:,:,2)	=	[1 0 0 1  ;0 1 0 0.4; 0 0 1 19.5;0 0 0 1];					% z=1
-projection_matrices(:,:,3)	=	[1 0 0 1.5;0 1 0 0.6; 0 0 1 19  ;0 0 0 1];					% z=2
-projection_matrices(:,:,4)	=	[1 0 0 2  ;0 1 0 0.8; 0 0 1 18.5;0 0 0 1];					% z=3
-projection_matrices(:,:,5)	=	[1 0 0 2.5;0 1 0 1  ; 0 0 1 18  ;0 0 0 1];					% z=4
-projection_matrices(:,:,6)	=	[1 0 0 3  ;0 1 0 1.2; 0 0 1 17.5;0 0 0 1];					% z=5
-projection_matrices(:,:,7)	=	[1 0 0 3.5;0 1 0 1.4; 0 0 1 17  ;0 0 0 1];					% z=6
-projection_matrices(:,:,8)	=	[1 0 0 4  ;0 1 0 1.6; 0 0 1 16.5;0 0 0 1];					% z=7
-projection_matrices(:,:,9)	=	[1 0 0 4.5;0 1 0 1.8; 0 0 1 16  ;0 0 0 1];					% z=8
-projection_matrices(:,:,10)	=	[1 0 0 5  ;0 1 0 2  ; 0 0 1 15.5;0 0 0 1];					% z=9
-projection_matrices(:,:,11)	=	[1 0 0 5.5;0 1 0 2.2; 0 0 1 15  ;0 0 0 1];					% z=10
-projection_matrices(:,:,12)	=	[1 0 0 6  ;0 1 0 2.4; 0 0 1 14.5;0 0 0 1];					% z=11
-projection_matrices(:,:,13)	=	[1 0 0 6.5;0 1 0 2.6; 0 0 1 14  ;0 0 0 1];					% z=12
-projection_matrices(:,:,14)	=	[1 0 0 7  ;0 1 0 2.8; 0 0 1 13.5;0 0 0 1];					% z=13
-projection_matrices(:,:,15)	=	[1 0 0 7.5;0 1 0 3  ; 0 0 1 13  ;0 0 0 1];					% z=14
-
-% view of the 3d box
-if debug_flag
-	scatter3( pts_on_cube(1, :), pts_on_cube(2, :), pts_on_cube(3, :) );
-	title( 'Points generated on a cube' );
-end
-  
-% loop for num of movements
-for nv = 1:num_views
-	% Move the box to be made visible to the camera.
-	rec_points				=	fcn_transformPoints( ...
-									projection_matrices(:, :, nv), pts_on_cube' );
-	point3d_actual(:, :, nv)=	rec_points;
-	point3d_noise(:, :, nv)	=	noise( rec_points, sigma );
-
-	% project to image frame
-	img_points				=	projectPoints( rec_points, K );
-	point2d(:, :, nv)		=	img_points;
-
-	if debug_flag & debug_impts
-		scatter( img_points(:,1), img_points(:,2) );
-		title( sprintf( 'Image points in view %d', nv ) );
-	end
-end
-
-% Ideally we will work on the noisy points.
-% Since the depthmaps we have produce noisy reconstructions.
-point3d						=	point3d_noise;
-
-% Now save data in a format that ceres understands.
-% helper for pointObservedValue
-point_cloud_helper			=	point3d(:, :, 1)';
-point_cloud_2d				=	point2d(:, :, 1)';
-for nv = 2:num_views
-	point_cloud_helper		=	[ point_cloud_helper point3d(:, :, nv)' ];
-	point_cloud_2d			=	[ point_cloud_2d point2d(:, :, nv)' ];
-end
-[ndims, npts_all] 			=	size( point_cloud_helper );
-point_cloud					=	zeros( ndims, num_pts );
-point_cloud( 1:3, 1:num_pts )=	point3d(:, :, 1)';
-
-
-% Finally collect all 2D-3D observations into one vector.
-for nv = 1:npts_all
-	point_obs_value(1, nv)	=	point_cloud_2d(1, nv);
-	point_obs_value(2, nv)	=	point_cloud_2d(2, nv);
-	point_obs_value(3, nv)	=	point_cloud_helper(1, nv);
-	point_obs_value(4, nv)	=	point_cloud_helper(2, nv);
-	point_obs_value(5, nv)	=	point_cloud_helper(3, nv);
-	point_obs_value(6, nv)	=	-0.1;
-end
-
-% The code does this same job as the for loop below. Just verify once.
-obs_matrix		=	zeros( num_pts, num_views );
-obs_matrix(:)	=	1:( num_pts * num_views );
-obs_matrix		=	obs_matrix';
-% count	=	1;
-% for nv1 = 1:num_views
-%     for nv2 = 1:num_pts
-%         A( nv1, nv2 )		=	count;
-%         count				=	count+1;
-%     end
+% noise_std	=	[1 2 3 4 5];																% Gaussian noise std in points.
+% dim_num		=	3;																			% Dimension
+% num_pts		=	1000;																		% number of points
+% seed = 123456789;
+% pts_scale	=	2.5;																		% Scale points with this factor after generation.
+% pts_on_cube	=	pts_scale * uniform_on_cube( dim_num, num_pts, seed );						% Generated random points on a cube.
+% sigma		=	noise_std( noise_num );
+% K			=	[718.856 0 607.1928; 0 718.856 185.2157; 0 0 1];							% Camera intrinsic parameters.
+% w3D 		=	10;																			% Some threshold passed to ceres.
+% 
+% debug_flag	=	0;
+% debug_impts	=	0;
+% num_views	=	15;																			% Number of camera views.
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% IGNORE THIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %     if matlabpool('size') ==0
+% %         try
+% %             matlabpool open 2;
+% %         catch
+% %         end
+% %     end
+% % random points generator on a cube
+% % system('rm myFile.txt');
+% % load('cube.mat');
+% % for num=1:size(t,2)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% IGNORE THIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% % Random intilization of the trajectories  without rotations 
+% projection_matrices(:,:,1)	=	[1 0 0 0.5;0 1 0 0.2; 0 0 1 20  ;0 0 0 1];					% z=0
+% projection_matrices(:,:,2)	=	[1 0 0 1  ;0 1 0 0.4; 0 0 1 19.5;0 0 0 1];					% z=1
+% projection_matrices(:,:,3)	=	[1 0 0 1.5;0 1 0 0.6; 0 0 1 19  ;0 0 0 1];					% z=2
+% projection_matrices(:,:,4)	=	[1 0 0 2  ;0 1 0 0.8; 0 0 1 18.5;0 0 0 1];					% z=3
+% projection_matrices(:,:,5)	=	[1 0 0 2.5;0 1 0 1  ; 0 0 1 18  ;0 0 0 1];					% z=4
+% projection_matrices(:,:,6)	=	[1 0 0 3  ;0 1 0 1.2; 0 0 1 17.5;0 0 0 1];					% z=5
+% projection_matrices(:,:,7)	=	[1 0 0 3.5;0 1 0 1.4; 0 0 1 17  ;0 0 0 1];					% z=6
+% projection_matrices(:,:,8)	=	[1 0 0 4  ;0 1 0 1.6; 0 0 1 16.5;0 0 0 1];					% z=7
+% projection_matrices(:,:,9)	=	[1 0 0 4.5;0 1 0 1.8; 0 0 1 16  ;0 0 0 1];					% z=8
+% projection_matrices(:,:,10)	=	[1 0 0 5  ;0 1 0 2  ; 0 0 1 15.5;0 0 0 1];					% z=9
+% projection_matrices(:,:,11)	=	[1 0 0 5.5;0 1 0 2.2; 0 0 1 15  ;0 0 0 1];					% z=10
+% projection_matrices(:,:,12)	=	[1 0 0 6  ;0 1 0 2.4; 0 0 1 14.5;0 0 0 1];					% z=11
+% projection_matrices(:,:,13)	=	[1 0 0 6.5;0 1 0 2.6; 0 0 1 14  ;0 0 0 1];					% z=12
+% projection_matrices(:,:,14)	=	[1 0 0 7  ;0 1 0 2.8; 0 0 1 13.5;0 0 0 1];					% z=13
+% projection_matrices(:,:,15)	=	[1 0 0 7.5;0 1 0 3  ; 0 0 1 13  ;0 0 0 1];					% z=14
+% 
+% % view of the 3d box
+% if debug_flag
+% 	scatter3( pts_on_cube(1, :), pts_on_cube(2, :), pts_on_cube(3, :) );
+% 	title( 'Points generated on a cube' );
 % end
-
-% Why is this done ? There is no need for icp since only the camera
-% matrix needs to be inverted to get this transformation!
-cameraRtC2W(:,:,1)			=	[eye(3),[0;0;0]];
-for nv = 1:num_views-1 
-	[R, T]					=	icp( point3d(:, : , nv)', point3d(:, :, nv+1 )' );
-	R_1						=	cameraRtC2W(:, 1:3, 1) * R;
-	T_1						=	cameraRtC2W(:, 4, 1) + R * T;
-	cameraRtC2W(:, :, nv+1)	=	[ R_1, T_1 ];
-end
-
-point_obs_index				=	sparse( obs_matrix );
-save('synthetic_data.mat', 'K', 'cameraRtC2W', 'point_cloud', ...
-			'point_obs_index', 'point_obs_value', 'w3D');
+%   
+% % loop for num of movements
+% for nv = 1:num_views
+% 	% Move the box to be made visible to the camera.
+% 	rec_points				=	fcn_transformPoints( ...
+% 									projection_matrices(:, :, nv), pts_on_cube' );
+% 	point3d_actual(:, :, nv)=	rec_points;
+% 	point3d_noise(:, :, nv)	=	noise( rec_points, sigma );
+% 
+% 	% project to image frame
+% 	img_points				=	projectPoints( rec_points, K );
+% 	point2d(:, :, nv)		=	img_points;
+% 
+% 	if debug_flag & debug_impts
+% 		scatter( img_points(:,1), img_points(:,2) );
+% 		title( sprintf( 'Image points in view %d', nv ) );
+% 	end
+% end
+% 
+% % Ideally we will work on the noisy points.
+% % Since the depthmaps we have produce noisy reconstructions.
+% point3d						=	point3d_noise;
+% 
+% % Now save data in a format that ceres understands.
+% % helper for pointObservedValue
+% point_cloud_helper			=	point3d(:, :, 1)';
+% point_cloud_2d				=	point2d(:, :, 1)';
+% for nv = 2:num_views
+% 	point_cloud_helper		=	[ point_cloud_helper point3d(:, :, nv)' ];
+% 	point_cloud_2d			=	[ point_cloud_2d point2d(:, :, nv)' ];
+% end
+% [ndims, npts_all] 			=	size( point_cloud_helper );
+% point_cloud					=	zeros( ndims, num_pts );
+% point_cloud( 1:3, 1:num_pts )=	point3d(:, :, 1)';
+% 
+% 
+% % Finally collect all 2D-3D observations into one vector.
+% for nv = 1:npts_all
+% 	point_obs_value(1, nv)	=	point_cloud_2d(1, nv);
+% 	point_obs_value(2, nv)	=	point_cloud_2d(2, nv);
+% 	point_obs_value(3, nv)	=	point_cloud_helper(1, nv);
+% 	point_obs_value(4, nv)	=	point_cloud_helper(2, nv);
+% 	point_obs_value(5, nv)	=	point_cloud_helper(3, nv);
+% 	point_obs_value(6, nv)	=	-0.1;
+% end
+% 
+% % The code does this same job as the for loop below. Just verify once.
+% obs_matrix		=	zeros( num_pts, num_views );
+% obs_matrix(:)	=	1:( num_pts * num_views );
+% obs_matrix		=	obs_matrix';
+% % count	=	1;
+% % for nv1 = 1:num_views
+% %     for nv2 = 1:num_pts
+% %         A( nv1, nv2 )		=	count;
+% %         count				=	count+1;
+% %     end
+% % end
+% 
+% % Why is this done ? There is no need for icp since only the camera
+% % matrix needs to be inverted to get this transformation!
+% cameraRtC2W(:,:,1)			=	[eye(3),[0;0;0]];
+% for nv = 1:num_views-1 
+% 	[R, T]					=	icp( point3d(:, : , nv)', point3d(:, :, nv+1 )' );
+% 	R_1						=	cameraRtC2W(:, 1:3, 1) * R;
+% 	T_1						=	cameraRtC2W(:, 4, 1) + R * T;
+% 	cameraRtC2W(:, :, nv+1)	=	[ R_1, T_1 ];
+% end
+% 
+% point_obs_index				=	sparse( obs_matrix );
+% save('synthetic_data.mat', 'K', 'cameraRtC2W', 'point_cloud', ...
+% 			'point_obs_index', 'point_obs_value', 'w3D');
 
 %% bundle ajustment with constarints
 % num_all=[0 50 100 200 300 400 500 600 700 800 900 1000];
@@ -145,7 +145,7 @@ num_constraints				=	length( constraints );
 for c_iter = 1:num_constraints
 	for m_iter = 1:length(mode)
 
-		load( 'synthetic_data.mat' ); 															% Make sure all 3D parameters are reset every iteration.
+		load( 'bundle_data_moving.mat' ); 															% Make sure all 3D parameters are reset every iteration.
 		num_of_rand_points		=	constraints( c_iter );
 		
 		% These labels don't mean much for the current experimentation.
@@ -156,31 +156,31 @@ for c_iter = 1:num_constraints
 		objectLabel.optimizationWeight	=	zeros(1,objectLabel.length);
 		
 		% Plot 3D points before doing bundle adjustment.
-		figure(1); scatter3( point_cloud(1, 1:num_pts), point_cloud(2, 1:num_pts), ...
-							 point_cloud(3, 1:num_pts) ); 
+		figure(1); scatter3( pointCloud(1, :), pointCloud(2, :), ...
+							 pointCloud(3, :) ); 
 							 title( 'Before Adjustment' );
-		axis( [-5 5 -6 6 0 30] );
+	%	axis( [-5 5 -6 6 0 30] );
 		
 		[ cameraRtC2W1, point_cloud ]	=	bundleAdjustment2D3DBoxFile( ...
-												cameraRtC2W, point_cloud, point_obs_index, ...
-												point_obs_value, K, w3D, mode(m_iter), num_of_rand_points);
+												cameraRtC2W, pointCloud, pointObserved, ...
+												pointObservedValue, K, w3D, mode(m_iter), num_of_rand_points);
 		
 		% Plot 3D points after doing bundle adjustment.
-		figure(2); scatter3( point_cloud(1, 1:num_pts), point_cloud(2, 1:num_pts), ...
-							 point_cloud(3, 1:num_pts) ); title( 'After Adjustment' );
-		axis( [-5 5 -6 6 0 30] );
+		figure(2); scatter3( point_cloud(1,:), point_cloud(2, :), ...
+							 point_cloud(3, :) ); title( 'After Adjustment' );
+		%axis( [-5 5 -6 6 0 30] );
 		
-		first							=	point3d_actual(:, :, 1)';
-		second							=	point_cloud(:, 1:num_pts );
-		err_reproj						=	0;
-		for err	= 1:num_pts
-			err_reproj					=	err_reproj + ...
-												sumsqr( first(:, err) - second(:, err) );
-		end
-		error_curr						=	[ mode(m_iter) sigma num_of_rand_points err_reproj ];
-		error_all( c_iter, : )			=	err_reproj;
+% 		first							=	point3d_actual(:, :, 1)';
+% 		second							=	point_cloud(:, 1:num_pts );
+% 		err_reproj						=	0;
+% 		for err	= 1:num_pts
+% 			err_reproj					=	err_reproj + ...
+% 												sumsqr( first(:, err) - second(:, err) );
+% 		end
+% 		error_curr						=	[ mode(m_iter) sigma num_of_rand_points err_reproj ];
+% 		error_all( c_iter, : )			=	err_reproj;
 		
 		% Finally append the error for the current iteration to a file
-		dlmwrite( save_filename, error_curr, '-append');
+	%	dlmwrite( save_filename, error_curr, '-append');
 	end
 end
